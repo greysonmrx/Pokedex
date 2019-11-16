7e7db3f728bb3d014157361da8c065fe9ce10045
greysonmrx <greysonmrx@gmail.com>
2019-11-16 13:46:26 -0300

    [CREATE] Style About

---
+++ src/components/About/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components/native';
+
+export const Description = styled.Text`
+    text-align: center;
+    line-height: 20;
+    width: 100%;
+    padding: 20px;
+    font-size: 15px;
+    border-bottom-color: rgba(0, 0, 0, .1);
+    border-bottom-width: 1px;
+`;
+
+export const Weight = styled.View`
+    align-items: center;
+    justify-content: center;
+    padding: 20px 60px;
+    border-left-color: rgba(0, 0, 0, .1);
+    border-left-width: 1px;
+`;
+
+export const Height = styled.View`
+    align-items: center;
+    justify-content: center;
+    padding: 20px 60px;
+`;
+
+export const StatsContent = styled.View`
+    width: 100%;
+    padding: 0px 20px;
+    flex-direction: row;
+    justify-content: center;
+    border-bottom-color: rgba(0, 0, 0, .1);
+    border-bottom-width: 1px;
+`;
+
+export const TitleDescription = styled.Text`
+    text-align: center;
+    width: 100%;
+    font-size: 16px;
+    font-weight: bold;
+    margin-top: 20px;
+`;
+
+export const HeightName = styled.Text`
+    font-size: 15px;
+    margin-bottom: 10px;
+`;
+
+export const HeightValue = styled.Text`
+    font-size: 15px;
+    font-weight: bold;
+    color: #000000;
+`;
+
+export const WeightName = styled.Text`
+    font-size: 15px;
+    margin-bottom: 10px;
+`;
+
+export const WeightValue = styled.Text`
+
+    font-size: 15px;
+    font-weight: bold;
+    color: #000000;
+`;
